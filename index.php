24cf8a91ae3406c179adf1ec0e8aaf09a234f2e0
vascovirgilio <69102097+vascovirgilio@users.noreply.github.com>
2022-12-03 12:34:37 +0200

    update index.php

--- index.php
+++ index.php
@@ -1,1 +1,12 @@
+<?php 
+require("src/transition.php");
+$mpesa = new Mpesa("56"); 
 
+$c2b = $mpesa->c2b([
+  'numero' => '846369717',
+  'valor' => '50',
+  'referencia' => 'testedePagamento'
+]);
+var_dump($c2b);
+
+?>
